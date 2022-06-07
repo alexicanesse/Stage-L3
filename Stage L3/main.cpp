4520317c32f6f73cf1f0a36566b473f96783650f
alexicanesse <alexi.canesse@protonmail.ch>
2022-06-07 07:44:26 +0100

    Initial Commit

---
+++ Stage L3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stage L3
+//
+//  Created by Alexi Canesse on 07/06/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
