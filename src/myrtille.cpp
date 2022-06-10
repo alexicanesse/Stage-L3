e47938dec60bace8d4810262c5fb3f5cd95d5e9c
alexicanesse <alexi.canesse@protonmail.ch>
2022-06-10 15:09:38 +0100

    implemented linear_quintille_using_threashold

--- src/myrtille.cpp
+++ src/myrtille.cpp
@@ -6,6 +6,30 @@
 //
 
 #include "myrtille.hpp"
+#include "AboveThreshold.hpp"
 
 #include <functional>
 
+double linear_quintille_using_threashold(std::vector<double> &db, int steps, double a, double b, double coef_quintille, double epsilon){
+    double a_copy = a;
+    
+    /* create the queries */
+    double lambda = (b-a)/steps;
+    std::vector<std::function<double(std::vector<double> &)>> Q;
+    while(steps--){
+        std::function<double(std::vector<double> &)> fi = { [a](std::vector<double> &db){
+            int count = 0;
+            for(auto e : db){
+                if(e < a)
+                    count++;
+            }
+            
+            return count;
+        } };
+        
+        a += lambda;
+        Q.push_back(fi);
+    }
+    
+    return a_copy + AboveThreshold(db, Q, coef_quintille*db.size(), epsilon)*lambda;
+}
