948e0fe781c9e0628e0b90323329006c63ca6b81
alexicanesse <alexi.canesse@protonmail.ch>
2022-06-10 14:48:11 +0100

    initial commit

---
+++ include/AboveThreshold.hpp
@@ -0,0 +1,16 @@
+//
+//  AboveThreshold.hpp
+//  Stage L3
+//
+//  Created by Alexi Canesse on 10/06/2022.
+//
+
+#ifndef AboveThreshold_hpp
+#define AboveThreshold_hpp
+
+#include <vector>
+#include <stdio.h>
+
+
+int AboveThreshold(std::vector<double> &db, std::vector<std::function<double(std::vector<double>&)>> f, double T, double epsilon);
+#endif /* AboveThreshold_hpp */
