081ecd116fcfd039debae82bbc043058a182e509
alexicanesse <alexi.canesse@protonmail.ch>
2022-06-10 13:39:09 +0100

    made a proper test

--- src/Inverse_sensitivity.cpp
+++ src/Inverse_sensitivity.cpp
@@ -6,12 +6,18 @@
 //
 
 #include "Inverse_sensitivity.hpp"
+#include "random.hpp"
+#include "exponential_mechanism.hpp"
+#include "common_tools.hpp"
 
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 
-int len_quantile(std::vector<float> x, float coef_quantile, float goal){
+extern class random rdm;
+
+int len_quantile(std::vector<double> x, double coef_quantile, double goal){
     /* do not pay the sorting part if the db is already sorted */
     if(!std::is_sorted(x.begin(), x.end()))
         std::sort(x.begin(), x.end());
@@ -30,3 +36,32 @@ int len_quantile(std::vector<float> x, float coef_quantile, float goal){
     
     return (int) std::abs(quantile_it - it) + 1;
 }
+
+
+
+
+void test_inverse_sensitivity(int n, double epsilon){
+    /*
+     * Test inverse sensitivity
+     */
+    std::vector<double> db = rdm.generate_db(n, [](){ return rdm.Laplace(1.); });
+//    double (*func)(double) = +[](double x){ return 1.; };
+//    double (*genere)(void) = +[](void){ return rdm.density(+[](double x){ return 0.5*exp(-abs(x)); }, 0.01, -100, 100); };
+//    std::vector<double> db = rdm.generate_db(n, genere);
+    
+    /* composition theorem */
+    epsilon /= 9;
+    
+    std::vector<double> result;
+    for(int i = 1; i < 10; ++i){
+        std::function<double(double)> func = { [i, db](double value){ return len_quantile(db, i*0.1, value); } };
+        result.push_back(exponential_mechanism(db, 0.01, -200, 200, func, epsilon));
+    }
+
+    
+    auto real_results = deciles(db);
+    std::cout << square_mean_error(result, real_results) << "\n";
+    for(int i = 0; i < 9; ++i)
+        std::cout << result.at(i) << "         "  << real_results.at(i) << "\n";
+}
+
